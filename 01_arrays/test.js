e5409710576d83bc1fc08ffa741a926cbf5d2acf
yadavsahil-sys <yadavsahil4730@gmail.com>
2026-04-28 00:38:44 +0530

    update array code

--- 01_arrays/test.js
+++ 01_arrays/test.js
@@ -1,2 +1,50 @@
-console.log("hey kg")
-console.log("yes sahil")
+//Arrays
+//const myArr  = [ 0,1,2,3,4,5]
+//const myHeros =["rohit","ishan"]
+//const myArr2 = new Array(1,2,3,4,5)
+//console.log(myArr[0]);
+
+//Array methods
+//myArr.push(6)
+//myArr.pop()
+//myArr.unshift(9)
+//myArr.shift()
+
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(3));
+//const newArr = myArr.join()
+
+//console.log(myArr);
+//console.log(typeof newArr);
+
+
+
+
+// slice,splice
+
+//console.log("A",myArr);
+
+//const myn1 = myArr.slice(1,3)
+
+//console.log(myn1);
+//console.log("B",myArr);
+
+//const myn2 = myArr.splice(1,3)                                
+//console.log("C", myArr);
+//console.log(myn2);
+
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+//marvel_heros.push(dc_heros)
+//console.log(marvel_heros)
+
+const allheros = marvel_heros.concat(dc_heros)
+console.log(allheros);
+
+
+
+
+
+
+
