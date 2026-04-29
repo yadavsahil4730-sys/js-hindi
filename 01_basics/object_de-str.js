3489e2b77ebfdf20b3d5151ded9453a2d19cf9d2
yadavsahil-sys <yadavsahil4730@gmail.com>
2026-04-30 01:08:32 +0530

    json talk

---
+++ 01_basics/object_de-str.js
@@ -0,0 +1,24 @@
+const course = {
+    coursename: "js in hindi",
+    price:"999",
+    courseInstructor: "kuku"
+
+}
+
+
+//course.courseInstructor
+
+const {courseInstructor} = course
+console.log(courseInstructor);
+
+//{
+//    "name": "sahil",
+//    "coursename": "js in hindi",
+//    "price": "free"
+//}
+
+[
+    {},
+    {},
+    {}
+]
