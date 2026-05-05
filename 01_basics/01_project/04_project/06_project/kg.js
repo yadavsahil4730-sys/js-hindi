a5215058295e6b487c8932ebefbf0692a0527da0
yadavsahil-sys <yadavsahil4730@gmail.com>
2026-05-06 01:00:28 +0530

    project 6 solution

---
+++ 01_basics/01_project/04_project/06_project/kg.js
@@ -0,0 +1,34 @@
+// Generate a random color
+
+const randomColor = function() {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+};
+
+let intervalId;
+
+function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+}
+
+const startChangingColor = function() {
+    if (!intervalId) {
+        intervalId = setInterval(changeBgColor, 1000);
+    }
+};
+
+const stopChangingColor = function() {
+    clearInterval(intervalId);
+    intervalId = null;
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
+
+
